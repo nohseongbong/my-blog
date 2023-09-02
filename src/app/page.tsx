0327bb89fd1bc29adae8aca395ce67d56296db3f
nohseongbong <shtjdqhd@naver.com>
2023-09-02 19:48:44 +0900

    del: 콘솔 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function App() {
 
 function Main() {
   const { data } = useQuery(["list"], () => getList());
-  console.log(data, ":data");
   return (
     <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
       <div className="bg-white p-10 rounded-3xl shadow-xl">
