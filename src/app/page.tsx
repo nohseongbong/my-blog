c0d7576af7f7bb4add3d8c4be14f8155ba33af1f
nohseongbong <shtjdqhd@naver.com>
2023-09-04 21:03:28 +0900

    feat: header 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,13 @@
 
 import { getList } from "@/api/list/list";
 import List from "@/component/list";
-import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
-export default function App() {
-  const queryClient = new QueryClient();
-
+export default function Home() {
+  const { data } = useQuery(["list"], getList);
   return (
-    <QueryClientProvider client={queryClient}>
-      <Main />
-    </QueryClientProvider>
-  );
-}
-
-function Main() {
-  const { data } = useQuery(["list"], () => getList());
-  return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
-      <div className="bg-white p-10 rounded-3xl shadow-xl">
+    <div className="py-20 px-10 grid gap-10 min-h-screen ">
+      <div className="p-10 rounded-3xl shadow-xl">
         <List list={data} />
       </div>
     </div>
