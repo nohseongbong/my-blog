66659980038fd5f142d812d659eca195f1698564
nohseongbong <shtjdqhd@naver.com>
2023-09-07 19:57:22 +0900

    feat: ssr 랜더링 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
-"use client";
+// "use client";
+
+// import { getList } from "@/api/list/list";
+// import List from "@/component/list";
+// import { useQuery } from "@tanstack/react-query";
+
+// export default function Home() {
+//   const { data } = useQuery(["list"], getList);
+//   return (
+//     <div className="py-20 px-10 grid gap-10 min-h-screen ">
+//       <div className="p-10 rounded-3xl shadow-xl">
+//         <List list={data} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// "use server";
+// SSG
 
 import { getList } from "@/api/list/list";
 import List from "@/component/list";
-import { useQuery } from "@tanstack/react-query";
 
-export default function Home() {
-  const { data } = useQuery(["list"], getList);
+export default async function Home() {
+  const data = await getList();
   return (
-    <div className="py-20 px-10 grid gap-10 min-h-screen ">
+    <div className="py-20 w-full px-10 grid  min-h-screen ">
       <div className="p-10 rounded-3xl shadow-xl">
         <List list={data} />
       </div>
     </div>
   );
 }
-// 테스트 page
