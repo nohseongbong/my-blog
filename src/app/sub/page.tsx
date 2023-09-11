cee05d92dcb494ed43d8ad48582ee404ceb50de9
nohseongbong <shtjdqhd@naver.com>
2023-09-11 19:25:18 +0900

    feat: 컴포넌트 추가

--- src/app/sub/page.tsx
+++ src/app/sub/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import ChildComponent from "@/component/sub/ChildComponent";
+
 export default function SubPage() {
   return (
     <div>
       <div>서브 페이지</div>
+      <ChildComponent />
     </div>
   );
 }
---
+++ src/component/sub/ChildComponent.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ChildComponent() {
+  return (
+    <div className="min-w-[400px] w-1/6 h-screen shadow-sm bg-blue">
+      자식 컴포넌트
+    </div>
+  );
+}
